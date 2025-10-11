6970fe6341a6f236dd2d933d5fc4a11770706fba
Earl Simbajon <justinesimbajon9@gmail.com>
2025-10-11 20:58:07 +0800

    Added ISO Standards in About

---
+++ apps/employee/.next/server/app/favicon.ico/route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/35f5b_next_31b3f1e0._.js")
+R.c("server/chunks/[root-of-the-server]__7876535b._.js")
+R.m("[project]/SmartGov/apps/employee/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
+R.m("[project]/SmartGov/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/SmartGov/apps/employee/src/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)")
+module.exports=R.m("[project]/SmartGov/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/SmartGov/apps/employee/src/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)").exports
