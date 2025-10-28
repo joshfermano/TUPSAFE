820f08bfbd675396101135f782f7d478505be76e
Josh Khovick Fermano <fermanojoshkhovick@gmail.com>
2025-10-28 18:12:06 +0800

    Merged" Resolved Merge Issues

--- apps/admin/.next/server/next-font-manifest.js
+++ apps/admin/.next/server/next-font-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/joshfermano/Thesis/smart-gov/apps/admin/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
-=======
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {},\n  \"appUsingSizeAdjust\": false,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
->>>>>>> 8ab018494a9a813622579248cec87fd9db791565
